cb1a442ead07a07535e5de0bb1afc717903c1225
Zack Cornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-04-07 14:18:00 +0800

    [Add] Added basic front/back-end layout

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
