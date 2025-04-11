25d67b4655aaaf7a228232a0519a10802ab597e2
Zack Cornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-04-11 12:10:26 +0800

    [Update] Added rating criteria to getPlaces function in Requests.js

--- backend/api/Requests.js
+++ backend/api/Requests.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const { PlacesClient } = require("@googlemaps/places");
 
 const placesClient = new PlacesClient({
   apiKey: process.env.GOOGLE_MAPS_API_KEY,
 });
 
-async function getPlaces(lat, long, radius, maxresults, types) {
+async function getPlaces(lat, long, radius, maxresults, types, rating) {
   const locationRestriction = {
     circle: {
       center: {
@@ -18,6 +20,7 @@ async function getPlaces(lat, long, radius, maxresults, types) {
     locationRestriction,
     maxResultCount: maxresults,
     includedTypes: types,
+    rating: rating,
   };
 
   try {
@@ -25,13 +28,35 @@ async function getPlaces(lat, long, radius, maxresults, types) {
       otherArgs: {
         headers: {
           "X-Goog-FieldMask":
-            "places.id,places.displayName,places.businessStatus",
+            "places.id,places.displayName,places.businessStatus,places.rating",
         },
       },
     });
+
+    console.log(
+      response[0].places.map((place) => {
+        return {
+          id: place.id,
+          name: place.displayName,
+          businessStatus: place.businessStatus,
+          rating: place.rating,
+        };
+      })
+    );
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
 async function getPlaceDetails() {}
+
+let types = ["restaurant"];
+
+getPlaces(
+  37.7749, // Latitude for San Francisco, CA
+  -122.4194, // Longitude for San Francisco, CA
+  5000, // Radius in meters
+  10, // Max results
+  types,
+  4.5 // Minimum rating
+);
