68de5149b8dd72f3cce4f92613b8729a736c2065
Adam Dabre <adamdabre23@gmail.com>
2025-04-16 14:36:11 +0800

    [Add] Search form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchPage from "./Pages/SearchPage";
+import MatchupPage from "./Pages/MatchupPage";
+import ResultPage from "./Pages/ResultPage";
 
 import "./App.css";
-import NavBar from "./components/NavBar";
+import Layout from "./Pages/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<NavBar />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<SearchPage />}></Route>
+                    <Route path="/matchup" element={<MatchupPage />}></Route>
+                    <Route path="/result" element={<ResultPage />}></Route>
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/SearchForm.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import styles from "./SearchForm.module.css";
+import getBrowserLocation from "../Helper/locationFunc";
+
+const SearchForm = () => {
+    return <section className={styles.searchFormContainer}>
+        
+    </section>;
+};
+
+export default SearchForm;
