629b8cf5512b25739691931593d73f7a93f70622
Adam Dabre <adamdabre23@gmail.com>
2025-04-16 14:34:29 +0800

    [Update] Updated NavBar component
    Changed the convention of class names

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,16 +3,16 @@ import { HashLink } from "react-router-hash-link";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
-  return (
-    <header className={styles["navbar"]}>
-      <nav className={styles["nav-container"]}>
-        <h1 className={styles["title"]}>Swipe n Dine</h1>
-        <NavLink to="/" className={styles["nav-link"]}>
-          Home
-        </NavLink>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.navbar}>
+            <nav className={styles.navContainer}>
+                <h1 className={styles.title}>Swipe n Dine</h1>
+                <NavLink to="/" className={styles.navLink}>
+                    Home
+                </NavLink>
+            </nav>
+        </header>
+    );
 };
 
 export default NavBar;
